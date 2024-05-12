366c44c999c4c73eef44cf90b06075426cfd0c9e
Benedikt Rötsch <benedikt@hashbite.net>
2024-05-12 12:44:18 -0300

    fix: add missing config for remote images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 export default {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "78.141.207.217",
+        port: "",
+        pathname: "/media/listing_images/**",
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
