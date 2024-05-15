7def94775907c697d4770b6937180480f69e22c0
Benedikt Rötsch <benedikt@hashbite.net>
2024-05-14 22:07:55 -0300

    fix: make header link to prototype actually centered

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -25,14 +25,14 @@ export default function Header() {
     <>
       {/* Header */}
       <div
-        className={`sticky z-10 flex h-[60px] justify-between items-center border-b !border-gray-100 bg-white px-4 transition-all duration-500 lg:h-20 lg:px-[166px] ${headerTopClass}`}
+        className={`sticky z-10 grid grid-cols-3 h-[60px] items-center border-b !border-gray-100 bg-white px-4 transition-all duration-500 lg:h-20 lg:px-[166px] ${headerTopClass}`}
       >
         {/* mybookr logo: Left side */}
         <Link className="cursor-pointer" href={"/"}>
           <Logo className="h-4 sm:h-6 w-auto" alt="mybookr logo" />
         </Link>
 
-        <Link className="font-bold" href={"/listings"}>Prototype</Link>
+        <Link className="font-bold text-center" href={"/listings"}>Prototype</Link>
 
         {/* Navigation: Right side */}
         <div className="flex justify-end">
