ccdf87966efc91ed8ab3c383e6acdead67375823
Benedikt Rötsch <benedikt@hashbite.net>
2024-06-10 11:41:00 +0200

    refactor: checkout page component split up

--- src/app/listings/[id]/checkout/CheckoutPage.tsx
+++ src/app/listings/[id]/checkout/CheckoutPage.tsx
@@ -28,6 +28,9 @@ import { graphql } from "@/gql";
 import { BookingContext } from "@/app/contexts/booking";
 import { useGetActivityFromExperience } from "@/app/helpers/useGetActivityFromExperience";
 import { useFormatPrice } from "@/app/helpers/useFormatPrice";
+import ViewConfirmation from "./ViewConfirmation";
+import ViewBookingForms from "./ViewBookingForms";
+import Sidebar from "./Sidebar";
 
 interface BookingStateStore {
   [key: string]: string;
@@ -84,7 +87,7 @@ export default function CheckoutPage({
 }: {
   experience: ExperienceItemFragment;
 }) {
-  const { activities, email } = useContext(BookingContext);
+  const { activities } = useContext(BookingContext);
   const activityId = activities[experience.id];
   const activity = useGetActivityFromExperience(activityId, experience);
 
@@ -122,12 +125,6 @@ export default function CheckoutPage({
     useMutation(CheckBookingMutation);
 
   const [clientSecret, setClientSecret] = React.useState<string | undefined>();
-  const stripe = React.useMemo(() => {
-    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-      throw new Error("Unable to init stripe without credentials");
-    }
-    return loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-  }, []);
 
   // Create new booking as soon we have availability and a booking flow token
   useEffect(() => {
@@ -202,20 +199,9 @@ export default function CheckoutPage({
 
   const isClient = useIsClient();
 
-  const formattedPrice = useFormatPrice(
-    activity?.availabilities
-      ? activity.availabilities[0].pricePerUnit
-      : undefined,
-    true,
-  );
-
-  const appearance: Appearance = {
-    theme: "stripe",
-  };
-  const options: StripeElementsOptions = {
-    clientSecret,
-    appearance,
-  };
+  const price = activity?.availabilities
+    ? activity.availabilities[0].pricePerUnit
+    : undefined;
 
   if (!isClient) {
     return null;
@@ -237,94 +223,19 @@ export default function CheckoutPage({
       <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-[2fr_minmax(min-content,480px)] xl:grid-cols-[2fr_minmax(min-content,600px)]">
         <div className="grid grid-cols-1 gap-16 px-0 py-8">
           {popupMessage && <strong>popupMessage: {popupMessage}</strong>}
-          {bookingUIState === "confirmation" && (
-            <div className="prose mx-auto text-center">
-              <h1>Booking Confirmed</h1>
-              <p>We have emailed details to {email}</p>
-            </div>
-          )}
-          {bookingUIState === "checkBookingStatus" && (
-            <div className="prose mx-auto text-center">
-              <CircularProgress size={48} /> <p>Double checking payment...</p>
-              <p>This should only take a few seconds!</p>
-            </div>
-          )}
-          {(bookingUIState === "bookingDetails" ||
-            bookingUIState === "providePaymentCredentials") && (
-            <BookingDataForm
-              experience={experience}
-              setBookingFormData={setBookingFormData}
-              bookingUIState={bookingUIState}
-            />
-          )}
-          {bookingUIState === "providePaymentCredentials" && clientSecret && (
-            <Elements options={options} stripe={stripe}>
-              <PaymentWrapper
-                experience={experience}
-                setPopupMessage={setPopupMessage}
-              />
-            </Elements>
-          )}
-          <BlaBla />
+          <ViewConfirmation bookingUIState={bookingUIState} />
+          <ViewBookingForms
+            bookingUIState={"bookingDetails"}
+            experience={experience}
+            setBookingFormData={setBookingFormData}
+            setPopupMessage={setPopupMessage}
+            clientSecret={clientSecret}
+          />
         </div>
 
         {/* Right section */}
         <div className="mr-40 hidden border-0 border-l border-r border-solid border-gray-100 md:block">
-          <div className="sticky top-5 grid gap-8 bg-white px-0 py-16">
-            {/* Villa Title */}
-            <Typography
-              className="!mb-4 p-0 !font-extrabold md:px-8 md:py-0 md:!text-2xl"
-              variant="h6"
-            >
-              {experience.title}
-            </Typography>
-
-            {/* Villa Image */}
-            {experience.medias &&
-              experience.medias.length > 0 &&
-              experience.medias[0].url && (
-                <div className="relative aspect-video w-full p-0 md:px-8 md:py-0">
-                  <Image
-                    className="rounded object-cover"
-                    src={experience.medias[0].url}
-                    alt=""
-                    fill={true}
-                  />
-                </div>
-              )}
-
-            {/* Price detail part */}
-            <div className="p-0 md:px-8 md:py-0">
-              <Typography
-                className="!mb-6 !text-base font-extrabold"
-                component="div"
-              >
-                Price details
-              </Typography>
-
-              {experience && <PriceDetail experience={experience} />}
-            </div>
-
-            <Divider />
-
-            {/* Total part */}
-            {formattedPrice && (
-              <div className="grid gap-4 p-0 md:px-8 md:py-0">
-                <div className="grid grid-cols-[1fr_max-content] items-center gap-2">
-                  <Typography variant="body2" className="!text-sm !font-black">
-                    Total
-                  </Typography>
-
-                  <Typography
-                    variant="body2"
-                    className="text-right !text-sm !font-black"
-                  >
-                    {formattedPrice}
-                  </Typography>
-                </div>
-              </div>
-            )}
-          </div>
+          <Sidebar experience={experience} price={price} />
         </div>
       </div>
     </>
