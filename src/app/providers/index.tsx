9dcc31748145b2d6f50c1b30ee355b8be90aed34
Benedikt Rötsch <benedikt@hashbite.net>
2024-06-15 21:48:24 +0200

    fix: make sure search state machine status is stored locally in the browser

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo } from "react";
 import {
   SearchMachineSnapshot,
   searchMachine,
@@ -8,47 +8,37 @@ import {
 } from "../state-machines/searchMachine";
 import { startOfToday, startOfTomorrow } from "date-fns";
 import { useMachine } from "@xstate/react";
-import { useIsClient } from "../helpers/useIsClient";
 
 export default function ContextProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const isClient = useIsClient();
-  // Handle search state machine with restore from local storage
-  const [initialBookingMachineSnapshot, setInitialBookingMachineSnapshot] =
-    useState<SearchMachineSnapshot>();
-  // useEffect(() => {
-  //   if (!isClient || initialBookingMachineSnapshot) {
-  //     return;
-  //   }
-  //   let dumbRestore;
-  //   try {
-  //     const entry = localStorage.getItem(`search-machine-state`);
-  //     if (!entry) {
-  //       return undefined;
-  //     }
-  //     dumbRestore = JSON.parse(entry);
-  //   } catch (err) {
-  //     console.log("broken search-machine-state in localStorage")
-  //     return undefined;
-  //   }
+  const initialBookingMachineSnapshot = useMemo(() => {
+    let dumbRestore;
+    try {
+      const entry = localStorage.getItem(`search-machine-state`);
+      if (!entry) {
+        return undefined;
+      }
+      dumbRestore = JSON.parse(entry);
+    } catch (err) {
+      console.log("broken search-machine-state in localStorage")
+      return undefined;
+    }
 
-  //   console.log("Could execute dumb restore:", dumbRestore);
-
-  //   setInitialBookingMachineSnapshot({
-  //     ...dumbRestore,
-  //     context: {
-  //       ...dumbRestore.context,
-  //       dateFrom: new Date(dumbRestore.context.dateFrom || startOfToday()),
-  //       dateTo: new Date(dumbRestore.context.dateTo || startOfTomorrow()),
-  //       bookingDate: new Date(
-  //         dumbRestore.context.bookingDate || startOfTomorrow(),
-  //       ),
-  //     },
-  //   } as SearchMachineSnapshot);
-  // }, [isClient, initialBookingMachineSnapshot]);
+    return {
+      ...dumbRestore,
+      context: {
+        ...dumbRestore.context,
+        dateFrom: new Date(dumbRestore.context.dateFrom || startOfToday()),
+        dateTo: new Date(dumbRestore.context.dateTo || startOfTomorrow()),
+        bookingDate: new Date(
+          dumbRestore.context.bookingDate || startOfTomorrow(),
+        ),
+      },
+    } as SearchMachineSnapshot;
+  }, []);
 
   const [searchMachineState, sendSearchMachineAction] = useMachine(
     searchMachine,
@@ -56,8 +46,6 @@ export default function ContextProviders({
       snapshot: initialBookingMachineSnapshot,
     },
   );
-
-  // console.log({searchMachineState, initialBookingMachineSnapshot})
 
   useEffect(() => {
     // Debugger for us
