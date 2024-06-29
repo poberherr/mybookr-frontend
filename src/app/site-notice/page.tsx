cff39db44cad64cc918c1c2bba0fb471dd9caa4c
Benedikt Rötsch <benedikt@hashbite.net>
2024-06-29 14:11:46 +0200

    feat: add updated site notice by Seb

--- src/app/site-notice/page.tsx
+++ src/app/site-notice/page.tsx
@@ -11,48 +11,65 @@ export default function PrivacyPolicyPage() {
           <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Site Notice (Impressum)
           </h1>
-
           <p>
-            P.A.M.M.M VENTURES LIMITED
-            <br />
-            APEC PLAZA
-            <br />
-            49 HOI YUEN ROAD KWUN TONG
-            <br />
-            Hong Kong
+            <strong>Company Name:</strong> MYBOOKR OPERATIONS INDONESIA
           </p>
-
-          <p> Business Registration Number: 74948082</p>
-
-          <h2>Contact</h2>
+          <p>
+            <strong>Address:</strong>
+            Jl. Raya Semat No. 17b, Tibubeennag, Kuta Utara, KABUPATEN BADUNG,
+            Bali, Indonesia
+          </p>
+          <h2 id="legal-entity-information-">Legal Entity Information:</h2>
           <ul>
             <li>
-              Phone: <a href="tel:+6282147456742">+62 82 147 4567 42</a>
+              <strong>Establishment Date:</strong> 31 May 2024
             </li>
             <li>
-              WhatsApp:{" "}
-              <a
-                href={`https://wa.me/6282147456742?text=${encodeURI("Hey mybookr.io team!")}`}
-                target="_blank"
-              >
-                +62 82 147 4567 42
-              </a>
+              <strong>Registration Number:</strong> 4024053151108515
             </li>
             <li>
-              E-mail: <a href="mailto:info@mybookr.io">info@mybookr.io</a>
+              <strong>Deed of Establishment Number:</strong> 264
+            </li>
+            <li>
+              <strong>Deed Date:</strong> 29 May 2024
+            </li>
+            <li>
+              <strong>Ministry Approval Number:</strong>{" "}
+              AHU-0038985.AH.01.01.TAHUN 2024
+            </li>
+            <li>
+              <strong>Approval Date:</strong> 31 May 2024
             </li>
           </ul>
-
-          <h2>Person responsible for editorial</h2>
-          <p>Sebastian Melz</p>
-
-          <h2>
-            Dispute resolution proceedings in front of a consumer arbitration
-            board
-          </h2>
+          <h2 id="tax-information-">Tax Information:</h2>
+          <ul>
+            <li>
+              <strong>Tax Registry Number (NPWP):</strong> 20.530.205.2-906.000
+            </li>
+            <li>
+              <strong>Tax Registry Date:</strong> 31 May 2024
+            </li>
+          </ul>
+          <h2 id="contact-information-">Contact Information:</h2>
+          <ul>
+            <li>
+              <strong>Email:</strong> info@mybookr.io
+            </li>
+          </ul>
+          <h2 id="disclaimer-">Disclaimer:</h2>
           <p>
-            We are not willing or obliged to participate in dispute resolution
-            proceedings in front of a consumer arbitration board.
+            MYBOOKR OPERATIONS INDONESIA (&quot;MyBookr.io&quot;) offers booking
+            software services and acts as an agent on behalf of clients in
+            various industries including hotel room bookings, villa bookings,
+            yacht bookings, diving school bookings, retreat bookings, and other
+            activity bookings. MyBookr.io is not the operator of these services
+            and is not responsible for the quality or availability of the
+            services themselves. Operators are responsible for ensuring that all
+            offerings, listings, and activities or rooms are available and meet
+            the presented quality standards.
+          </p>
+          <p>
+            For any questions or concerns, please contact us at info@mybookr.io.
           </p>
         </div>
       </div>
