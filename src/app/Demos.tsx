e52fdec31d6287943fb7965f31c14e378a15ae8a
Benedikt Rötsch <benedikt@hashbite.net>
2024-06-29 15:00:39 +0200

    feat: better wording for demos

--- src/app/Demos.tsx
+++ src/app/Demos.tsx
@@ -23,11 +23,11 @@ export default function Demos() {
         <div className="mx-auto max-w-2xl text-center">
           <a id="demos" />
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Our Demos
+            Our Product Demos
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            To ensure our software works for the whole industry, we currently
-            have two variants of our end product.
+            To make our software suitable for the entire industry, we currently
+            offer two versions of our end product.
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
@@ -49,7 +49,7 @@ export default function Demos() {
               <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
                 <Link href={post.href}>
                   <span className="absolute inset-0" />
-                  {post.title}
+                  Try the {post.title} <span aria-hidden="true">→</span>
                 </Link>
               </h3>
             </article>
