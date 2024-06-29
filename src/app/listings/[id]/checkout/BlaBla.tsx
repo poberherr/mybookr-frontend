8d46016df693ccff8b895b94f2960dc344a2412a
Benedikt Rötsch <benedikt@hashbite.net>
2024-06-29 14:52:40 +0200

    feat: rework blabla text below booking

--- src/app/listings/[id]/checkout/BlaBla.tsx
+++ src/app/listings/[id]/checkout/BlaBla.tsx
@@ -19,19 +19,16 @@ const BlaBla = () => (
       </Typography>
 
       <Typography variant="body1">
-        Free cancellation within 48 hours. If you cancel before the 5th Sept.
-        you'll receive a prorated refund.
-      </Typography>
-
-      <Typography className="!mt-1 cursor-pointer underline" variant="body2">
-        Learn more
+        Currently, we do not offer online cancellations. Please ensure you are
+        certain about your booking. If you need to cancel for any reason, please
+        contact us, and we will connect you directly with the Operator/Host.
       </Typography>
     </div>
 
     <Divider />
 
     {/* Calendar */}
-    <div className="flex items-center gap-8 px-4 py-0 md:pl-40 md:pr-16">
+    {/* <div className="flex items-center gap-8 px-4 py-0 md:pl-40 md:pr-16">
       <CalendarIcon className="h-8 w-8" alt={"calendar icon"} />
 
       <Typography variant="subtitle1" component="p">
@@ -40,36 +37,27 @@ const BlaBla = () => (
       </Typography>
     </div>
 
-    <Divider />
+    <Divider /> */}
 
     {/* Agreement */}
     <div className="px-4 py-0 md:pl-40 md:pr-16">
       <div>
         <Typography className="!text-gray-400" variant="caption" component="p">
           By clicking the button below, I agree to the{" "}
-          <span className="font-bold text-black">host's house rules,</span>{" "}
+          <span className="text-black">operators house rules</span>,{" "}
           <Link
-            className="font-bold text-black underline"
+            className="text-black underline"
             href="/terms-of-service#refund-and-dispute-policy"
             target="_blank"
           >
             mybookr.io's Refunds and Dispute Policy
-          </Link>{" "}
-          and confirm that mybookr agrees to my payment method can charge and to
-          whom I am responsible for damages. Total amount is to be paid when the
-          host/hostess confirms my booking request.
+          </Link>
+          , and confirm that mybookr can charge my payment method and that I am
+          responsible for any damages.
         </Typography>
 
         <Typography
-          className="mt-2 !text-gray-400"
-          component="p"
-          variant="caption"
-        >
-          The terms of payment between you and mybookr apply.
-        </Typography>
-
-        <Typography
-          className="mt-2 !text-gray-400"
+          className="!mt-4 !text-gray-400"
           component="p"
           variant="caption"
         >
@@ -95,7 +83,7 @@ const BlaBla = () => (
             href="/terms-of-service#security-capabilities-and-policy-for-transmission-of-payment-card-details"
             target="_blank"
           >
-            payment services
+            Payment Services
           </Link>{" "}
           and agree to the payment services{" "}
           <Link
@@ -103,8 +91,8 @@ const BlaBla = () => (
             href="/privacy-policy#payment-services"
             target="_blank"
           >
-            privacy policy.
-          </Link>{" "}
+            Privacy Policy.
+          </Link>
         </Typography>
       </div>
     </div>
