ad7b8a1e7db4c5b7d173c0b90828ce5c6ca485e2
Benedikt Rötsch <benedikt@hashbite.net>
2024-06-29 14:20:06 +0200

    feat: add small fancy background blur

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="sticky inset-0 z-10 bg-white">
+    <header className="sticky inset-0 z-10 bg-white bg-opacity-90 backdrop-blur-lg">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
