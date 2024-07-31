ca4471c9ee88ac9ad44d6c837ae6099f33410abd
Benedikt Rötsch <benedikt@hashbite.net>
2024-07-31 13:18:06 +0200

    chore: remove all stripe related code

--- src/app/listings/[id]/checkout/ViewBookingForms.tsx
+++ src/app/listings/[id]/checkout/ViewBookingForms.tsx
@@ -1,18 +1,10 @@
 import React, { useEffect, useRef } from "react";
 
-import { Elements } from "@stripe/react-stripe-js";
-
 import BlaBla from "./BlaBla";
 import FormBookingDetails, { BookingFormData } from "./FormBookingDetails";
-import FormPayment from "./FormPayment";
 import { ExperienceItemFragment } from "@/gql/graphql";
 import { StateValueFrom } from "xstate";
 
-import {
-  Appearance,
-  StripeElementsOptions,
-  loadStripe,
-} from "@stripe/stripe-js";
 import { IBookingContext, bookingMachine } from "./bookingMachine";
 import { CircularProgress } from "@mui/material";
 
@@ -31,37 +23,9 @@ const ViewConfirmation = ({
   experience,
   setError,
 }: IProps) => {
-  const stripe = React.useMemo(() => {
-    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-      throw new Error("Unable to init stripe without credentials");
-    }
-    return loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-  }, []);
-
-  const appearance: Appearance = {
-    theme: "stripe",
-  };
-  const options: StripeElementsOptions = {
-    clientSecret: context.clientSecret,
-    appearance,
-  };
-  const paymentWrapperRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (value === "ProvidePaymentCredentials") {
-      setTimeout(() => {
-        paymentWrapperRef.current &&
-          paymentWrapperRef.current.scrollIntoView({
-            behavior: "smooth",
-          });
-      }, 300);
-    }
-  }, [value, paymentWrapperRef.current]);
-
   return (
     <>
-      {(value === "BookingDetails" ||
-        value === "ProvidePaymentCredentials") && (
+      {(value === "BookingDetails") && (
         <FormBookingDetails
           experience={experience}
           submit={submit}
@@ -75,15 +39,6 @@ const ViewConfirmation = ({
           <h1>Updating Booking</h1>
         </div>
       )}
-      <div ref={paymentWrapperRef}>
-        {value === "ProvidePaymentCredentials" &&
-          stripe &&
-          context.clientSecret && (
-            <Elements options={options} stripe={stripe}>
-              <FormPayment setError={setError} />
-            </Elements>
-          )}
-      </div>
       <BlaBla />
     </>
   );
