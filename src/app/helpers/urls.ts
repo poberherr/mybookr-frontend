cb532b92a25ef53ed8488394fd7006bf2ef6ee20
Benedikt Rötsch <benedikt@hashbite.net>
2024-08-08 14:02:29 +0200

    fix: fix url path generation hook

--- src/app/helpers/urls.ts
+++ src/app/helpers/urls.ts
@@ -3,8 +3,5 @@ import { decodeGlobalId } from "./global-ids";
 import { ExperienceItemFragment } from "@/gql/graphql";
 
 export const useExperienceURL = (experience: ExperienceItemFragment) => {
-  const url = useMemo(() => {
-    return `/listings/${decodeGlobalId(experience.id).id}-${experience.slug}`;
-  }, []);
-  return url
+  return `/listings/${decodeGlobalId(experience.id).id}-${experience.slug}`;
 }
